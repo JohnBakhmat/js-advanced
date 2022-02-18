25e8733aea25d75941b1f7675cf6ce5a27d47068
Yevhenii Bakhmat <yevhenii.bakhmat@nure.ua>
2022-02-18 21:37:11 +0200

    Marked up the default labcard

---
+++ src/components/LabCard.vue
@@ -0,0 +1,12 @@
+<template>
+  <article class="wrap">
+    <div class="content">lab1</div>
+  </article>
+</template>
+<style lang="scss" scoped>
+.wrap {
+  aspect-ratio: 1;
+  border-radius: 20px;
+  background: rgba(110, 107, 142, 0.4);
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,36 @@
 <template>
-  <div class="root">1</div>
+  <div class="root">
+    <div class="wrap">
+      <LabCard />
+      <LabCard />
+      <LabCard />
+    </div>
+  </div>
 </template>
 
-<script lang="ts"></script>
+<script lang="ts">
+import { defineComponent } from "vue";
+import LabCard from "@/components/LabCard.vue";
+
+export default defineComponent({
+  components: { LabCard },
+});
+</script>
 
 <style lang="scss" scoped>
 .root {
   background: linear-gradient(125deg, #0d324d, #7f5a83);
   height: 100vh;
   width: 100vw;
-  place-items: center;
   display: grid;
+  place-items: center;
+}
+.wrap {
+  display: grid;
+  place-items: center;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 100px;
+  max-width: 80vw;
+  width: 100%;
 }
 </style>
