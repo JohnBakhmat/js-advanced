9d5bc744a27acfe09460938e8576c29d58c00c85
Yevhenii <yevhenii.bakhmat@nure.ua>
2022-02-19 22:10:18 +0200

    Add wave between articles

--- src/views/FirstLabView.vue
+++ src/views/FirstLabView.vue
@@ -4,13 +4,15 @@
       <Lab1Input v-model="user" :label="label" />
       <ArticleArrow />
     </article>
-    <article>123421</article>
+    <basic-wave />
+    <article id="tasks_3-4" class="root">123421</article>
   </div>
 </template>
 <script lang="ts">
 import { computed, defineComponent, ref } from "vue";
 import Lab1Input from "@/components/Lab1Input.vue";
 import ArticleArrow from "@/components/ArticleArrow.vue";
+import BasicWave from "@/components/BasicWave.vue";
 
 export default defineComponent({
   setup() {
@@ -26,7 +28,13 @@ export default defineComponent({
   components: {
     Lab1Input,
     ArticleArrow,
+    BasicWave,
   },
 });
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+#tasks_3-4 {
+  background: #222;
+  color: white;
+}
+</style>
