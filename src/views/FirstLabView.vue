a9b8d65f110542014a3a96af49d784feeef0230b
Yevhenii Bakhmat <yevhenii.bakhmat@nure.ua>
2022-02-21 13:12:29 +0200

    Created Breed Button and modal component

--- src/views/FirstLabView.vue
+++ src/views/FirstLabView.vue
@@ -1,4 +1,5 @@
 <template>
+  <AppModal />
   <div class="wrap">
     <section id="tasks_1-2" class="root">
       <Lab1Input v-model="user" :label="label" />
@@ -6,6 +7,12 @@
     </section>
     <basic-wave />
     <section id="tasks_3-5" class="root">
+      <div>
+        <breed-button classes="random-breed" @click="handleRandomBreedClick">
+          <i class="fas fa-paw"></i>
+          Random breed
+        </breed-button>
+      </div>
       <ul class="breed-list">
         <bread-item
           v-for="(subBreeds, breed, index) in breeds"
@@ -24,6 +31,8 @@ import ArticleArrow from "@/components/ArticleArrow.vue";
 import BasicWave from "@/components/BasicWave.vue";
 import axios from "axios";
 import BreadItem from "@/components/BreadItem.vue";
+import BreedButton from "@/components/BreedButton.vue";
+import AppModal from "@/components/AppModal.vue";
 export default defineComponent({
   setup() {
     //Refs
@@ -38,6 +47,10 @@ export default defineComponent({
     //Functions
     const scrollTo = (id: string) => {
       document.querySelector(`#${id}`)?.scrollIntoView({ behavior: "smooth" });
+    };
+
+    const handleRandomBreedClick = () => {
+      console.log("asdf");
     };
     const fetchBreeds = () => {
       axios.get("https://dog.ceo/api/breeds/list/all").then((res) => {
@@ -55,6 +68,7 @@ export default defineComponent({
       user,
       scrollTo,
       breeds,
+      handleRandomBreedClick,
     };
   },
   components: {
@@ -62,6 +76,8 @@ export default defineComponent({
     ArticleArrow,
     BasicWave,
     BreadItem,
+    BreedButton,
+    AppModal,
   },
 });
 </script>
@@ -75,9 +91,13 @@ section {
 #tasks_3-5 {
   background: #222;
   color: white;
+  display: grid;
+  place-items: center;
+  grid-template-columns: 1fr 1fr 1fr;
 }
 .breed-list {
-  display: flex;
+  grid-column: 2;
+  display: grid;
   flex-direction: column;
   gap: 15px;
   max-height: 100vh;
@@ -85,5 +105,24 @@ section {
   width: 100%;
   align-self: center;
   overflow-y: auto;
+}
+.btn {
+  background: grey;
+  padding: 10px 20px;
+  border-radius: 10px;
+  font-size: 1.5rem;
+}
+.random-breed {
+  background: #008864;
+}
+
+//Easter egg
+.random-breed-cursed {
+  font-weight: bolder;
+  background: center / cover no-repeat
+    url("https://media.wired.co.uk/photos/606d9c691e0ddb19555fb809/16:9/w_2992,h_1683,c_limit/dog-unsolicited.jpg");
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
+  color: black;
 }
 </style>
