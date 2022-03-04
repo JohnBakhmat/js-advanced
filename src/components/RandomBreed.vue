b447abf8f91244309adc9676a38746cd3afed3a5
Yevhenii <yevhenii.bakhmat@nure.ua>
2022-03-04 14:52:07 +0200

    Add subbreed counter

--- src/components/RandomBreed.vue
+++ src/components/RandomBreed.vue
@@ -1,23 +1,33 @@
 <template>
   <div id="random-breed-modal">
     <div class="text-column">
-      <h1 class="breed-name">Title</h1>
+      <h1 class="breed-name">{{ breed }}</h1>
+      <div class="subbreed-count">
+        <h2>Sub Breeds:</h2>
+        <h2>{{ subBreeds.length }}</h2>
+      </div>
 
       <ul>
         <li v-for="(breed, index) in subBreeds" :key="index">Hound</li>
       </ul>
     </div>
-    <img src="https://images.dog.ceo/breeds/mix/Cali-Mini-Labradoodle.jpg" />
+    <img :src="breedImg" />
   </div>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 
 export default defineComponent({
   setup() {
+    const breed = ref("Breed");
     const subBreeds = new Array(50);
+    const breedImg = computed(() => {
+      return "https://images.dog.ceo/breeds/mix/Cali-Mini-Labradoodle.jpg";
+    });
     return {
+      breed,
       subBreeds,
+      breedImg,
     };
   },
 });
@@ -31,12 +41,30 @@ export default defineComponent({
   width: 100%;
   height: 100%;
   justify-items: left;
+  color: white;
 }
 .text-column {
   width: 100%;
   display: grid;
+  grid-template-columns: 1fr;
   gap: 20px;
 }
+.subbreed-count {
+  width: 100%;
+  align-items: center;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  padding: 0 10px;
+  h2 {
+    font-size: 1.25rem;
+    &:last-child {
+      font-weight: 700;
+      font-size: 1.75rem;
+      justify-self: end;
+    }
+  }
+}
+
 .breed-name {
   text-align: center;
   place-self: center;
@@ -49,7 +77,6 @@ img {
 ul {
   max-height: 50vh;
   overflow: auto;
-  color: white;
   list-style-type: disc;
   border-radius: 10px;
   padding: 20px 30px;
